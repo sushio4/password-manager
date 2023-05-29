e67c1d5267f304edddec06d9a299b5f5113d4143
Maciej Suski <suskimaciej@interia.pl>
2023-05-29 20:15:47 +0200

    Added AES type enum

--- src/Safe.hpp
+++ src/Safe.hpp
@@ -5,11 +5,22 @@
 #include "aes.hpp"
 #include "hack_private.hpp"
 
+enum AESType{
+    AES_128,
+    AES_192,
+    AES_256,
+    AES_128_CBC,
+    AES_192_CBC,
+    AES_256_CBC
+};
+
 class Safe{
 private:
     std::string name;
     std::unique_ptr<AES> cipher;
+    AESType type;
 public:
     hackPrivate(name);
     hackPrivate(cipher);
+    hackPrivate(type);
 };
