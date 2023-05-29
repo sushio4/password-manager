fe29ae5867988466179d71d2c06bc18e9a16a036
Maciej Suski <suskimaciej@interia.pl>
2023-05-29 18:44:47 +0200

    Changed it for easier implementation of gui someday

--- src/Safes.hpp
+++ src/Safes.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <memory>
 
 #include "Sync.hpp"
@@ -19,7 +20,8 @@ private:
 public:
     SafesModule(std::shared_ptr<SyncModule>& syncRef, std::shared_ptr<CipherModule>& cipherRef);
 
-    const Safe& getSafe(std::string name);
+    std::string getDecryptedSafe(std::string name);
+    void getSafeList(std::vector<std::string>& list);    
     bool writeSafe(const Safe& safe, std::string name);
     bool synchronize(void);
 
