42117022da6cd3e8fb8e12855b8a99c307ee3b66
Maciej Suski <suskimaciej@interia.pl>
2023-05-29 18:45:27 +0200

    Began implementing Manager methods

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -9,4 +9,34 @@ Manager::Manager(void)
     generator = std::make_unique<GeneratorModule>(safes);
     login = std::make_unique<LoginModule>(sync, cipher);
 
+    logged = false;
+}
+
+void Manager::getSafeList(std::vector<std::string>& list) const
+{
+    safes->getSafeList(list);
+}
+
+std::string Manager::readPassword(const std::string& safeName) 
+{
+    return safes->getDecryptedSafe(safeName);
+}
+
+std::string Manager::generatePassword(const std::vector<std::string>& args)
+{
+    return generator->generate(args);
+}
+
+bool Manager::existsSafe(const std::string& name)
+{
+    std::vector<std::string> list;
+    safes->getSafeList(list);
+    for(auto s : list)
+        if(s == name) return true;
+    return false;
+}
+
+bool Manager::editSafe(const std::string& safeName, const std::vector<std::string>& data)
+{
+    git
 }
