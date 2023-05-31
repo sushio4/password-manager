42706f768c851467682da695968f3f95472d9bd1
Maciej Suski <suskimaciej@interia.pl>
2023-05-31 10:39:53 +0200

    Planned CipherModule's insides
    Don't hesitate to modify or replace it as you see fit

--- src/Cipher.hpp
+++ src/Cipher.hpp
@@ -1,8 +1,24 @@
 #pragma once
+#include <stdint.h>
+#include <string>
 
-//#include "AES/AES.hpp"
+#include "aes.hpp"
 
 class CipherModule
 {
+private:
+    uint8_t* masterKey = nullptr;
+    bool validated = false;
 
+public:
+    //it'll generate masterKey  from password
+    //try to decrypt something
+    //and return whether decryption was successfull
+    bool validatePassword(const std::string& password); 
+    
+    //as this is the only class containing masterKey
+    //it will be (en/de)crypting everything with those
+    //using functions from inside the AES object
+    bool decrypt(AES& src, std::string& dest);
+    bool encrypt(const std::string& src, AES& dest);
 };
