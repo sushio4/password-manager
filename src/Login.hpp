8c2eb73cfbf794a82d2b5a89895c77912fbc1839
Maciej Suski <suskimaciej@interia.pl>
2023-06-01 10:11:34 +0200

    Changes in resource management
    - const references as arguments
    - cache for remote credentials

--- src/Login.hpp
+++ src/Login.hpp
@@ -11,11 +11,16 @@ private:
     std::shared_ptr<CipherModule> cipher;
     std::shared_ptr<SyncModule> sync;
 
+    //So that the user does not have to enter them every single time
+    std::string email;
+    std::string password;
+
 public:
     LoginModule(std::shared_ptr<SyncModule>& syncRef, std::shared_ptr<CipherModule>& cipherRef);
 
-    bool login(std::string password);
-    bool changePassword(std::string oldPassword, std::string newPassword);
-    bool changeEmail(std::string password, std::string email);
+    bool login(const std::string& password);
+    bool loginRemote(const std::string& email, const std::string& password);
+    bool changePassword(const std::string& oldPassword, const std::string& newPassword);
+    bool changeEmail(const std::string& password, const std::string& email);
     bool synchronize(void);
 };
